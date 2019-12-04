fc9a9ca807abf2df7bf3250402a4b5e125eeb985
Mario <mariofronza1@gmail.com>
2019-12-04 16:11:14 -0300

    feat: Routes to create, list and delete tools

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
+const ToolController = require('./app/controllers/ToolController')
+
 const routes = Router()
 
-routes.get('/', () => {
-  console.log('Hello World')
-})
+routes.get("/tools", ToolController.index)
+routes.post("/tools", ToolController.store)
+routes.delete("/tools/:id", ToolController.destroy)
 
 module.exports = routes
