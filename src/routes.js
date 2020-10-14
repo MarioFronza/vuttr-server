ab0957410dff42482cdafacc0371eae7ebec3cc5
Mario Fronza <mariofronza1@gmail.com>
2020-10-14 17:10:54 -0300

    feat: remove tools store route

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const ToolController = require('./app/controllers/ToolController')
 const routes = Router()
 
 // routes.get("/tools", ToolController.index)
-routes.post("/tools", ToolController.store)
+// routes.post("/tools", ToolController.store)
 routes.delete("/tools/:id", ToolController.destroy)
 
 module.exports = routes
